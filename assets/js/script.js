27c2e3f830cdcc7d1f9e417a0687055c6301f513
tumelo-maja <mogale.ramsey@gmail.com>
2025-02-27 21:30:08 +0000

    Add Favicon, and final tidy up for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame('addition');
 })
 
@@ -25,6 +30,10 @@ document.addEventListener("DOMContentLoaded", function() {
      * and after the user's answer has been processed
      */
 function runGame(gameType){
+    
+    document.getElementById('answer-box').value="";
+    document.getElementById('answer-box').focus();
+
     
     // create 2 random numbers
     let num1 = Math.floor(Math.random()*25)+1;
