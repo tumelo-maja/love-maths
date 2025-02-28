23c83f8b07890d5aa06d3eb8810b6fdd99d21d20
tumelo-maja <mogale.ramsey@gmail.com>
2025-02-28 10:24:24 +0000

    Add code for division grame

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,12 +45,14 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === 'subtract') {
         displaySubtractionQuestion(num1, num2);
-    } 
-    else{
+    } else if (gameType === 'division') {
+        displayDivideQuestion(num1, num2);
+    } else{
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
+    
 }
 
 /**
@@ -89,6 +91,8 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"]
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"]
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"]
     } else {
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}. Aborting!`
@@ -130,4 +134,11 @@ function displayMultiplyQuestion(operand1,operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivideQuestion(operand1,operand2){
+
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand1;
+    document.getElementById('operator').textContent = "/";
 }
